c0e0594c5cbf8cfbbdfdd913acb8dc2cfb3672a5
Edgar Elihu GR <edgar.elihu.garcia.ramirez@gmail.com>
2022-08-16 15:55:19 -0500

    -Created img nodes and added src

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 // Avocados API
-const url = 'https://platzi-avo.vercel.app/api/avo';
+const apiURL = 'https://platzi-avo.vercel.app/api/avo';
+const domainURL = 'https://platzi-avo.vercel.app'; 
 let allItems = [];
 // Fetch data (async-await)
 async function fetchData(){
-    const response = await fetch(url);
+    const response = await fetch(apiURL);
     const data = await response.json();
-    allItems = await [...data.data];
-    allItems.forEach((item) => console.log(item.name));
+    allItems = await data.data;
+    allItems.forEach((item) => {
+        console.log(item.name);
+        const image = document.createElement('img');
+        image.src = `${domainURL}${item.image}`;
+
+        document.body.append(image);
+    })
 }
 
 fetchData();
