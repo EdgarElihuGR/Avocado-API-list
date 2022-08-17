ddec823462bb29e78e478edd7ca7a82c4522f1ae
Edgar Elihu GR <edgar.elihu.garcia.ramirez@gmail.com>
2022-08-17 15:57:29 -0500

    -Added price, description to avocado element
    -Optimized DOM rendering with document fragment implementation

--- src/index.js
+++ src/index.js
@@ -11,13 +11,30 @@ async function fetchData(){
     const response = await fetch(apiURL);
     const data = await response.json();
     allItems = await data.data;
-    allItems.forEach((item) => {
-        console.log(item.name);
-        const image = document.createElement('img');
-        image.src = `${domainURL}${item.image}`;
+    // Create DOM fragment to avoid live DOM rendering multiple times
+    let domFragment = document.createDocumentFragment();
 
-        document.body.append(image);
+    allItems.forEach((item) => {
+        const imageNode = document.createElement('img');
+        imageNode.src = `${domainURL}${item.image}`;
+        const priceNode = document.createElement('h2');
+        priceNode.setAttribute('class', 'text-stone-900 text-2xl');
+        priceNode.textContent = item.price;
+        const descriptionNode = document.createElement('p');
+        descriptionNode.textContent = item.attributes.description;
+        descriptionNode.setAttribute("class", "mt-4 max-w-2xl text-xl leading-7 text-gray-500");
+        const detailsContainer = document.createElement('div');
+        detailsContainer.setAttribute('class', 'details-container');
+        detailsContainer.setAttribute('class', 'px-5');
+        detailsContainer.append(priceNode, descriptionNode);
+        const container = document.createElement('div');
+        container.setAttribute("class", "container");
+        container.append(imageNode, detailsContainer);
+        // Append new container to fragment (Not the live DOM)
+        domFragment.append(container);
     })
+    // Append the fragment to the live DOM just once (More optimal)
+    document.body.append(domFragment);
 }
 
 fetchData();
