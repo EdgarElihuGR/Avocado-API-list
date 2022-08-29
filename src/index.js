c5b4f72f1f6b6bba3735928cdf99fe94b503b924
Edgar Elihu GR <edgar.elihu.garcia.ramirez@gmail.com>
2022-08-29 11:14:21 -0500

    -Added styles to app container and details container, added grid layout

--- src/index.js
+++ src/index.js
@@ -12,24 +12,33 @@ async function fetchData(){
     const data = await response.json();
     allItems = await data.data;
     const appNode = document.querySelector('#app');
+    appNode.className = 'grid xl:grid-cols-2 gap-4 justify-center px-3';
     // Create DOM fragment to avoid live DOM rendering multiple times
     let domFragment = document.createDocumentFragment();
 
     allItems.forEach((item) => {
         const imageNode = document.createElement('img');
         imageNode.src = `${domainURL}${item.image}`;
+        imageNode.className = 'max-w-xs max-h-80 rounded-full p-5';
         const priceNode = document.createElement('h2');
-        priceNode.setAttribute('class', 'text-stone-900 text-2xl');
+        
+        /* Diff ways to set styles */
+        // priceNode.setAttribute('class', 'text-stone-900 text-2xl');
+        // priceNode.style = 'font-size: 3rem';
+        // priceNode.style.fontSize = '3rem';
+        priceNode.className = 'title-text';
+
         priceNode.textContent = item.price;
+        priceNode.style.color = '#779422';
         const descriptionNode = document.createElement('p');
         descriptionNode.textContent = item.attributes.description;
-        descriptionNode.setAttribute("class", "mt-4 max-w-2xl text-xl leading-7 text-gray-500");
+        descriptionNode.className = "mt-4 max-w-2xl text-lg leading-7 text-gray-500";
         const detailsContainer = document.createElement('div');
         detailsContainer.setAttribute('class', 'details-container');
-        detailsContainer.setAttribute('class', 'px-5');
+        detailsContainer.setAttribute('class', 'px-6 py-3');
         detailsContainer.append(priceNode, descriptionNode);
         const container = document.createElement('div');
-        container.setAttribute("class", "container");
+        container.className = "container flex flex-column rounded-3xl hover:bg-gray-300 ease-in duration-200";
         container.append(imageNode, detailsContainer);
         // Append new container to fragment (Not the live DOM)
         appNode.append(container);
